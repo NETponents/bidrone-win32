df97381bc050c1cdeea0904fea4a48968114aaea
ARMmaster17 <wordman05@gmail.com>
2015-05-28 11:58:07 -0400

    Base commit + ScreenManager()

---
+++ bidrone/bidrone/Screen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bidrone
+{
+    public partial class Screen
+    {
+        public Screen()
+        {
+
+        }
+        public virtual void Update()
+        {
+
+        }
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ bidrone/bidrone/ScreenManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bidrone
+{
+    public class ScreenManager
+    {
+        Dictionary<string, Screen> screens = new Dictionary<string, Screen>();
+        string curScreen;
+
+        public ScreenManager()
+        {
+
+        }
+        public void Update()
+        {
+            screens[curScreen].Update();
+        }
+        public void Draw()
+        {
+            screens[curScreen].Draw();
+        }
+        public void setScreen(string newScreen)
+        {
+            //TODO: check to see if screen exists in screens<>
+            curScreen = newScreen;
+        }
+        public void createScreen(string name, Screen newScreen)
+        {
+            screens.Add(name, newScreen);
+        }
+        public Screen getScreen()
+        {
+            return screens[curScreen];
+        }
+        public Screen getScreen(string scrn)
+        {
+            return screens[scrn];
+        }
+        public string getScreenName()
+        {
+            return curScreen;
+        }
+    }
+}
